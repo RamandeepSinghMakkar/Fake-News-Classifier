ce7e77d83bc53a26474755a6d2850d59bef8debb
Ramandeep Singh Makkar <ramandeepsinghmakkar199@gmail.com>
2024-12-08 23:17:48 +0530

    Delete feedback.sql

--- feedback.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE feedback (
-    feedback_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    feedback_text TEXT NOT NULL,
-    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
